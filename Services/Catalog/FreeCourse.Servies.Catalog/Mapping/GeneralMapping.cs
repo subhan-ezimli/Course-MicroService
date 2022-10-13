3e7122baaf77fdd2e1fa790a238a20a4c21db58d
subhan-ezimli <subhanav@code.edu.az>
2022-10-13 20:16:53 +0400

    #autoMapper

---
+++ Services/Catalog/FreeCourse.Servies.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FreeCourse.Servies.Catalog.Dtos;
+using FreeCourse.Servies.Catalog.Model;
+
+namespace FreeCourse.Servies.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+
+        }
+    }
+}
--- Services/Catalog/FreeCourse.Servies.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Servies.Catalog/Program.cs
@@ -1,11 +1,15 @@
+using FreeCourse.Servies.Catalog.Mapping;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddAutoMapper(typeof(GeneralMapping));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
