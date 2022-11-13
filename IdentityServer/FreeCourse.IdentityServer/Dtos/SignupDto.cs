acdd82b697b80d76013d8059ff3a064962a81802
subhan-ezimli <subhanav@code.edu.az>
2022-11-13 23:20:48 +0400

    #PhotoStock Controller

---
+++ IdentityServer/FreeCourse.IdentityServer/Dtos/SignupDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.IdentityServer.Dtos
+{
+    public class SignupDto
+    {
+        [Required(ErrorMessage ="Istifadəçi adı daxil edin")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = " E-Poçt daxil edin")]
+        public string  Email { get; set; }
+
+        [Required(ErrorMessage = " Password daxil edin")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = " Şəhər daxil edin")]
+        public string City  { get; set; }
+    }
+}
--- Services/Catalog/FreeCourse.Servies.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Servies.Catalog/Program.cs
@@ -1,15 +1,29 @@
 using FreeCourse.Servies.Catalog.Mapping;
 using FreeCourse.Servies.Catalog.Services;
 using FreeCourse.Servies.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+{
+    x.Authority = builder.Configuration["IdentityServerUrl"];
+    x.Audience = "resource_catalog";
+    x.RequireHttpsMetadata = false;
+}
+    );
 builder.Services.AddScoped<ICategoryService,CategoryService>();
 builder.Services.AddScoped<ICourseService,CourseService>();
 builder.Services.AddAutoMapper(typeof(GeneralMapping));
-
+builder.Services.AddControllers(x =>
+{
+    x.Filters.Add(new AuthorizeFilter());
+});
 builder.Services.AddControllers();
 
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
@@ -22,6 +36,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,9 +45,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllers();
+app.MapControllers(); 
 
 app.Run();
