1c29e975b3cbffd93ea17fe994817c2d99498ff4
subhan-ezimli <subhanav@code.edu.az>
2022-11-15 00:57:18 +0400

    PhotoSave Method created

--- PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
+++ PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FreeCourse.Services.PhotoStock.Controllers
-{
+{   
     [Route("api/[controller]")]
     [ApiController]
     public class PhotosController : CustomBaseController
@@ -19,8 +19,9 @@ namespace FreeCourse.Services.PhotoStock.Controllers
                 var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
 
                 using (var stream = new FileStream(path, FileMode.Create))
+                
                     await photo.CopyToAsync(stream, cancellationToken);
-
+                
                 var returnPath = "photos/" + photo.FileName;
 
                 PhotoDto photoDto=new () { Url=returnPath };
@@ -30,5 +31,19 @@ namespace FreeCourse.Services.PhotoStock.Controllers
 
             return CreateActionResultInstance (Response<PhotoDto>.Fail("photo is empty",400));
         }
+
+        [HttpPost]
+        public async Task<IActionResult> PhotoDelete(string photoUrl)
+        {
+            var path=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/photos",photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+
     }
 }
--- PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+{
+    x.Authority = builder.Configuration["IdentityServerUrl"];
+    x.Audience = "photo_stock_catalog";
+    x.RequireHttpsMetadata = false;
+}
+    );
+builder.Services.AddControllers(x =>
+{
+    x.Filters.Add(new AuthorizeFilter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +30,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
